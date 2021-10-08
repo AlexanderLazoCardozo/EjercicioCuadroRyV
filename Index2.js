9488d4a4986e760a63150342c7aa3e7fdf0aa0b9
AlexanderLazoCardozo <lazoalexander86@gmail.com>
2021-10-08 12:23:55 -0500

    EjercicioCuadrados_AlumnoLazo

---
+++ Index2.js
@@ -0,0 +1,13 @@
+function desplegar1() {
+    const cuadror = document.getElementById('rojo')
+    const cuadrov = document.getElementById("verde")
+    cuadror.style.display = 'block'
+    cuadrov.style.display ="none"
+}
+
+function desplegar2() {
+    const cuadrov = document.getElementById("verde")
+    const cuadror = document.getElementById('rojo')
+    cuadrov.style.display ="block"
+    cuadror.style.display = 'none'
+}
